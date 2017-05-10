462a1d0544938d4e67f90c6f7c626418bae0a1b4
Aramatena <915343815@qq.com>
2017-05-10 17:30:14 +0800

    delete some values in string.xml

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -961,13 +961,13 @@ public final class R {
         public static final int abc_shareactionprovider_share_with_application = 0x7f060011;
         public static final int abc_toolbar_collapse_description = 0x7f060012;
         public static final int appbar_scrolling_view_behavior = 0x7f060028;
-        public static final int bottom_sheet_behavior = 0x7f06002b;
-        public static final int character_counter_pattern = 0x7f06002c;
-        public static final int password_toggle_content_description = 0x7f06003f;
-        public static final int path_password_eye = 0x7f060040;
-        public static final int path_password_eye_mask_strike_through = 0x7f060041;
-        public static final int path_password_eye_mask_visible = 0x7f060042;
-        public static final int path_password_strike_through = 0x7f060043;
+        public static final int bottom_sheet_behavior = 0x7f06002a;
+        public static final int character_counter_pattern = 0x7f06002b;
+        public static final int password_toggle_content_description = 0x7f06003a;
+        public static final int path_password_eye = 0x7f06003b;
+        public static final int path_password_eye_mask_strike_through = 0x7f06003c;
+        public static final int path_password_eye_mask_visible = 0x7f06003d;
+        public static final int path_password_strike_through = 0x7f06003e;
         public static final int search_menu_title = 0x7f060013;
         public static final int status_bar_notification_info_overflow = 0x7f060014;
     }
