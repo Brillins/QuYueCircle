d4b13edb4525bcffc7005bf97749b1c2989a75b1
Aramatena <915343815@qq.com>
2017-06-03 14:53:20 +0800

    refactor

--- app/src/main/java/com/lovejoy/activity/MainActivity.java
+++ app/src/main/java/com/lovejoy/activity/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends FragmentActivity implements
 	/**
 	 * Called when the activity is first created.
 	 */
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
